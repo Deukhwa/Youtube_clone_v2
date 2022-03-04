2be7139219369e810e14ce96dcb49f7009d3dfd9
Deukhwa <deukhwajeong@gmail.com>
2022-03-04 14:37:21 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- localhost/VideoTube -->
+<!DOCTYPE html>
+<html>
+  <head>    
+    <title>VideoTube</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  </head>
+  <body>
+    <div id="pageContainer">
+      <div id="mastHeadContainer">
+        Helloo
+      </div>
+      <div id="sideNavContainer">
+
+      </div>
+
+      <div id="mainSectionContainer">
+
+      </div>
+    </div>
+  </body>
+</html>
