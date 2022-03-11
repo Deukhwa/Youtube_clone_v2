6a780e4ea5e4215edced571eb714a98e9a559a93
Deukhwa <deukhwajeong@gmail.com>
2022-03-10 19:50:20 -0500

    Finish inserting the video data into the table

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -30,7 +30,12 @@ class VideoProcessor {
       
       if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
         
-        $finalFilePath = $targetDir
+        $finalFilePath = $targetDir . uniqid() . ".mp4";
+
+        if(!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+          echo "Insert query failed";
+          return false;
+        }
 
       }
   }
@@ -65,6 +70,19 @@ class VideoProcessor {
 
   private function hasError($data) {
     return $data["error"] != 0;
+  }
+
+  private function insertVideoData($uploadData, $filePath) {
+    $query = $this->con->prepare("INSERT INTO videos(title, uploadedBy, description, privacy, category, filePath) 
+                                              VALUES(:title, :uploadedBy, :description, :privacy, :category, :filePath)");
+    $query->bindParam(":title", $uploadData->title);
+    $query->bindParam(":uploadedBy", $uploadData->uploadedBy);
+    $query->bindParam(":description", $uploadData->description);
+    $query->bindParam(":privacy", $uploadData->privacy);
+    $query->bindParam(":category", $uploadData->category);
+    $query->bindParam(":filePath", $filePath);
+
+    return $query->execute();
   }
 }
 
